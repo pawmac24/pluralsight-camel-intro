2059ae48800c7424e8dc0ed727093c05d4088ec5
Pawel Mackiewicz PGS <pmackiewicz@pgs-soft.com>
2016-09-06 17:03:54 +0200

    Forth  stage - activemq config

--- src/main/java/com/pluralsight/orderfulfillment/config/IntegrationConfig.java
+++ src/main/java/com/pluralsight/orderfulfillment/config/IntegrationConfig.java
@@ -1,6 +1,10 @@
 package com.pluralsight.orderfulfillment.config;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.activemq.pool.PooledConnectionFactory;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsConfiguration;
 import org.apache.camel.component.sql.SqlComponent;
 import org.apache.camel.spring.javaconfig.CamelConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +14,7 @@ import org.springframework.core.env.Environment;
 import com.pluralsight.orderfulfillment.order.OrderStatus;
 
 import javax.inject.Inject;
+import javax.jms.ConnectionFactory;
 import javax.sql.DataSource;
 
 import java.util.ArrayList;
@@ -28,6 +33,31 @@ public class IntegrationConfig extends CamelConfiguration {
 	private DataSource dataSource;
 
 	@Bean
+	public ConnectionFactory jmsConnectionFactory() {
+		return new ActiveMQConnectionFactory(environment.getProperty("activemq.broker.url"));
+	}
+	
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public PooledConnectionFactory pooledConnectionFactory() {
+		PooledConnectionFactory factory = new PooledConnectionFactory();
+		factory.setConnectionFactory(jmsConnectionFactory());
+		factory.setMaxConnections(Integer.parseInt(environment.getProperty("pooledConnectionFactory.maxConnections")));
+		return factory;
+	}
+	
+	@Bean
+	public JmsConfiguration jmsConfiguration(){
+		JmsConfiguration jmsConfiguration = new JmsConfiguration();
+		jmsConfiguration.setConnectionFactory(pooledConnectionFactory());
+		return jmsConfiguration;
+	}
+	
+	@Bean
+	public ActiveMQComponent activeMQ(){
+		ActiveMQComponent activeMQ = new ActiveMQComponent();
+		activeMQ.setConfiguration(jmsConfiguration());
+		return activeMQ;
+	}
 	public SqlComponent sql() {
 		SqlComponent sqlComponent = new SqlComponent();
 		sqlComponent.setDataSource(dataSource);
@@ -50,7 +80,7 @@ public class IntegrationConfig extends CamelConfiguration {
 								+ OrderStatus.PROCESSING.getCode() + "'"
 								+ " where id = :#id")
 						.beanRef("orderItemMessageTranslator", "transformToOrderItemMessage")
-						.to("log:com.pluralsight.orderfulfillment.order?level=INFO");
+						.to("activemq:queue:ORDER_ITEM_PROCESSING");
 			}
 		};
 
