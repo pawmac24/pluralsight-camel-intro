b4d41dd3f1f57e4335813fd0ae69634a7623cf82
Pawel Mackiewicz PGS <pmackiewicz@pgs-soft.com>
2016-09-07 15:04:55 +0200

    Fifth stage - Content based Router

--- src/main/java/com/pluralsight/orderfulfillment/config/IntegrationConfig.java
+++ src/main/java/com/pluralsight/orderfulfillment/config/IntegrationConfig.java
@@ -4,6 +4,7 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.activemq.pool.PooledConnectionFactory;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.xml.Namespaces;
 import org.apache.camel.component.jms.JmsConfiguration;
 import org.apache.camel.component.sql.SqlComponent;
 import org.apache.camel.spring.javaconfig.CamelConfiguration;
@@ -85,6 +86,40 @@ public class IntegrationConfig extends CamelConfiguration {
 						.to("activemq:queue:ORDER_ITEM_PROCESSING");
 			}
 		};
-
+	}
+	
+	/**
+	 * <Order xmlns="http://www.pluralsight.com/orderfullfilment/Order">
+	 * <OrderType><FulfillmentCenter>ABCFulfillmentCenter</FulfillmentCenter>
+	 * @return
+	 */
+	@Bean
+	public RouteBuilder fullfillmentCenterContentBasedRouter() {
+		return new RouteBuilder() {
+			
+			@Override
+			public void configure() throws Exception {
+				Namespaces namespace = new Namespaces("o", "http://www.pluralsight.com/orderfulfillment/Order");
+				
+				//Send from ORDER_ITEM_PROCESSING queue to the correct
+				//fulfillment center queue.
+				from("activemq:queue:ORDER_ITEM_PROCESSING")
+					.choice()
+					.when()
+					.xpath("/o:Order/o:OrderType/o:FulfillmentCenter = '"
+							+ com.pluralsight.orderfulfillment.generated.FulfillmentCenter.ABC_FULFILLMENT_CENTER.value()
+							+ "'", namespace)
+					.to("activemq:queue:ABC_FULLFILMENT_REQUEST")
+					
+					.when()
+					.xpath("/o:Order/o:OrderType/o:FulfillmentCenter = '"
+							+ com.pluralsight.orderfulfillment.generated.FulfillmentCenter.FULFILLMENT_CENTER_ONE.value()
+							+ "'", namespace)
+					.to("activemq:queue:FC1_FULLFILMENT_REQUEST")
+					
+					.otherwise()
+					.to("activemq:queue:ERROR_FULLFILMENT_REQUEST");				
+			}
+		};
 	}
 }
